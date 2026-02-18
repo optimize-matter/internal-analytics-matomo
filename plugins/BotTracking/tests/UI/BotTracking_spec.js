6d37d940ee94efbcc7d46f86e5fcf26e34755dbc
Nathan Gavin <nathangavin987@gmail.com>
2026-02-19 03:59:39 +1300

    update all references of ai assistants and agent (#24062)
    * update all references of ai assistants and agent
    
    * remove other lang files
    
    * phpcs
    
    * fix generated update
    
    * move update to core
    
    * update UI tests
    
    * change update script to be more specific when invalidating archives
    
    * reorder AI assistant subcategories
    
    * update dashboard widget list width to fit longer string
    
    * update UI and system tests
    
    * update system expected files
    
    * update system expected files
    
    * update submodule references
    
    * update submodule references
    
    * update submodule references
    
    * update submodule references
    
    * update expected test files for 7.2 tests
    
    * update expected test files, remove todays date
    
    * update submodules
    
    * resolve merge conflicts
    
    * require correct class
    
    * fix UI tests after merge
    
    * Fix footer message test
    
    * provide more detail in migration hint
    
    * reset some test files to resolve conflict
    
    * bring back changed expected files after conflict resolution
    
    * fix system expected files again
    
    * reset some expected files
    
    * reset some expected files
    
    * split invalidations into separate migrations
    
    * Apply suggestions from code review
    
    * only invalidate all visits segment
    
    * updates expected UI file
    
    * updates expected UI file
    
    ---------
    
    Co-authored-by: Matt <1169490+caddoo@users.noreply.github.com>
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- plugins/BotTracking/tests/UI/BotTracking_spec.js
+++ plugins/BotTracking/tests/UI/BotTracking_spec.js
@@ -15,8 +15,8 @@ describe("BotTracking", function () {
     var generalParams = 'idSite=1&period=day&date=2025-02-02',
         urlBase = 'module=CoreHome&action=index&' + generalParams;
 
-    it('should render AI Assistants > AI Bots Overview page with evolution and sparkline', async function () {
-        await page.goto("?" + urlBase + "#?" + generalParams + "&category=General_AIAssistants&subcategory=BotTracking_AIBotsOverview");
+    it('should render AI Assistants > AI Chatbots Overview page with evolution and sparkline', async function () {
+        await page.goto("?" + urlBase + "#?" + generalParams + "&category=General_AIAssistants&subcategory=BotTracking_AIChatbotsOverview");
         await page.waitForNetworkIdle();
 
         await page.hover('.jqplot-seriespicker');
@@ -34,7 +34,7 @@ describe("BotTracking", function () {
     });
 
     it('should not show unique pages and documents metric for higher periods', async function () {
-        await page.goto("?" + urlBase + "#?idSite=1&period=week&date=2025-02-02&category=General_AIAssistants&subcategory=BotTracking_AIBotsOverview");
+        await page.goto("?" + urlBase + "#?idSite=1&period=week&date=2025-02-02&category=General_AIAssistants&subcategory=BotTracking_AIChatbotsOverview");
         await page.waitForNetworkIdle();
 
         await page.hover('.jqplot-seriespicker');
@@ -46,8 +46,8 @@ describe("BotTracking", function () {
         expect(sparklines.length).to.equal(6);
     });
 
-    it('should render AI Assistants > AI Bots Overview bot detail report', async function () {
-        await page.goto("?" + urlBase + "#?" + generalParams + "&category=General_AIAssistants&subcategory=BotTracking_AIBotsOverview");
+    it('should render AI Assistants > AI Chatbots Overview bot detail report', async function () {
+        await page.goto("?" + urlBase + "#?" + generalParams + "&category=General_AIAssistants&subcategory=BotTracking_AIChatbotsOverview");
         await page.waitForNetworkIdle();
 
         const row = await page.jQuery('tr.subDataTable:first');
@@ -57,7 +57,7 @@ describe("BotTracking", function () {
         await page.waitForNetworkIdle();
         await page.waitForTimeout(250); // rendering
 
-        var elem = await page.$('#widgetBotTrackinggetAIAssistantRequests');
+        var elem = await page.$('#widgetBotTrackinggetAIChatbotRequests');
         expect(await elem.screenshot()).to.matchImage('bot_requests');
     });
 
@@ -72,13 +72,13 @@ describe("BotTracking", function () {
         await page.waitForNetworkIdle();
         await page.waitForTimeout(250); // rendering
 
-        var elem = await page.$('#widgetBotTrackinggetAIAssistantRequests');
+        var elem = await page.$('#widgetBotTrackinggetAIChatbotRequests');
         expect(await elem.screenshot()).to.matchImage('bot_requests_documents');
     });
 
     it('should show segment not supported footer message in AI bot reports when segmented', async function () {
         const segment = encodeURIComponent('visitConverted==1');
-        await page.goto("?" + urlBase + "#?" + generalParams + "&category=General_AIAssistants&subcategory=BotTracking_AIBotsOverview&segment=" + segment);
+        await page.goto("?" + urlBase + "#?" + generalParams + "&category=General_AIAssistants&subcategory=BotTracking_AIChatbotsOverview&segment=" + segment);
         await page.waitForNetworkIdle();
 
         const expectedMessage = 'Report does not support segmentation. The data displayed is your standard, unsegmented report data.';
